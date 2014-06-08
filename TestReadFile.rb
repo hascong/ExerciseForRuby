7f17c151562a125828c3803d97c52832d76d1fdf
dynamiclc <dynamiclc@gmail.com>
2014-06-08 22:49:16 +0900

    Update TestReadFile.rb

--- TestReadFile.rb
+++ TestReadFile.rb
@@ -2,25 +2,25 @@
 #encoding: UTF-8  
 
 # Test case 1
-system "cat \"Test Case 1  The new word is a new head\n\""
+system "cat \"Test Case 1  The new word is a new head\n\" > test_result_read_file_rb"
 system "clear && ruby ReadFile.rb boy \"Little boy.\" >> test_result_read_file_rb"
 
 # Test case 2
-system "cat \"\nTest Case 2  The new word is behind current head\n\""
+system "cat \"\nTest Case 2  The new word is behind current head\n\" >> test_result_read_file_rb"
 system "clear && ruby ReadFile.rb goddd \"Goddd.\" >> test_result_read_file_rb"
 
 # Test case 3
-system "cat \"\nTest Case 3  The new word is equal to certain entry\n\""
+system "cat \"\nTest Case 3  The new word is equal to certain entry\n\" >> test_result_read_file_rb"
 system "clear && ruby ReadFile.rb good \"Good to see you again.\" \"Good night baby.\" \"Good evening.\" >> test_result_read_file_rb"
 
 # Test case 4
-system "cat \"\nTest Case 4  The new word is behind the middle\n\""
+system "cat \"\nTest Case 4  The new word is behind the middle\n\" >> test_result_read_file_rb"
 system "clear && ruby ReadFile.rb goooooood \"Goooooood.\" >> test_result_read_file_rb"
 
 # Test case 5
-system "cat \"\nTest Case 5  The new word is before tail\n\""
+system "cat \"\nTest Case 5  The new word is before tail\n\" >> test_result_read_file_rb"
 system "clear && ruby ReadFile.rb hi \"Hi there.\" \"Hi I am John.\" >> test_result_read_file_rb"
 
 # Test case 6
-system "cat \"\nTest Case 6  The new word is behind current tail\n\""
+system "cat \"\nTest Case 6  The new word is behind current tail\n\" >> test_result_read_file_rb"
 system "clear && ruby ReadFile.rb zip \"Zip!\" \"Zip up.\" >> test_result_read_file_rb"
